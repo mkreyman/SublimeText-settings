0cc67ed2af11fda0afbee8a4ffca0499dc351ae0
Mark Kreyman <mark@kreyman.com>
2017-03-10 10:34:53 -0700

    Added a script to copy settings

---
+++ cp_subl_settings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings ~/workspace/SublimeText-settings/
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ~/workspace/SublimeText-settings/
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Terminal/Terminal.sublime-settings ~/workspace/SublimeText-settings/
